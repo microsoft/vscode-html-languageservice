dbf98623a5564f92c0ad118722d905bccbfec498
Martin Aeschlimann <martinae@microsoft.com>
2019-03-27 10:26:53 +0100

    format

--- src/htmlLanguageService.ts
+++ src/htmlLanguageService.ts
@@ -64,6 +64,6 @@ export function getLanguageService(options?: LanguageServiceOptions): LanguageSe
 	};
 }
 
-export function newHTMLDataProvider(id: string, customData: HTMLDataV1) : IHTMLDataProvider {
+export function newHTMLDataProvider(id: string, customData: HTMLDataV1): IHTMLDataProvider {
 	return new HTMLDataProvider(id, customData);
 }
