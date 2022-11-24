17b0c2dc31878608ac4a466b17f9f537f3da70cb
Martin Aeschlimann <martinae@microsoft.com>
2022-11-24 19:35:14 +0100

    update dependencies & prepare 5.0.3

--- src/beautify/beautify-html.js
+++ src/beautify/beautify-html.js
@@ -1,5 +1,5 @@
 // copied from js-beautify/js/lib/beautify-html.js
-// version: 1.14.6
+// version: 1.14.7
 /* AUTO-GENERATED. DO NOT MODIFY. */
 /*
 
@@ -2408,7 +2408,7 @@ Beautifier.prototype._get_tag_open_token = function(raw_token) { //function to g
 
   parser_token.is_unformatted = !parser_token.tag_complete && in_array(parser_token.tag_check, this._options.unformatted);
   parser_token.is_content_unformatted = !parser_token.is_empty_element && in_array(parser_token.tag_check, this._options.content_unformatted);
-  parser_token.is_inline_element = in_array(parser_token.tag_name, this._options.inline) || parser_token.tag_start_char === '{';
+  parser_token.is_inline_element = in_array(parser_token.tag_name, this._options.inline) || parser_token.tag_name.includes("-") || parser_token.tag_start_char === '{';
 
   return parser_token;
 };
